d710c68860e21cb906ac382e9e552d9a1f77af05
fatima kassem <fatima.kassem@buil4all.net>
2026-06-11 14:50:47 +0300

    tutorial vd and logo in login

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
     defaultConfig {
         applicationId = "com.build4alluat.build4allmanager"
-     //minSdk = (flutter.minSdkVersion as Number).toInt()
+    // minSdk = (flutter.minSdkVersion as Number).toInt()
      minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
